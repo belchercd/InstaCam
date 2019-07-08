12e7f4890ecb935eddbc827e8eac89b9b1be16a5
Camryn Belcher <belchercd@fb.com>
2019-07-08 16:33:55 -0700

    Initial Commit

---
+++ InstaCam/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InstaCam
+//
+//  Created by belchercd on 7/8/19.
+//  Copyright © 2019 belchercd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
